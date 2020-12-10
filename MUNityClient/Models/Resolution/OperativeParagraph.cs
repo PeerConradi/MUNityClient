aafd0d7dfdd65e1b026e553a0984f43a2ce80dce
Aeuke Peer Conradi <peerconradi@gmail.com>
2020-12-10 22:10:32 +0100

    Little Bug Fixes

--- MUNityClient/Models/Resolution/OperativeParagraph.cs
+++ MUNityClient/Models/Resolution/OperativeParagraph.cs
@@ -7,11 +7,32 @@ namespace MUNityClient.Models.Resolution
 {
     public class OperativeParagraph : IOperativeParagraph
     {
+        public delegate void OnTextChanged(OperativeParagraph paragraph, string newText, string oldText);
+
+        public event OnTextChanged TextChanged;
+
         public string OperativeParagraphId { get; set; }
         public string Name { get; set; } = "";
         public bool IsLocked { get; set; } = false;
+
+        /// <summary>
+        /// Virtual is true when the Operative Paragraph comes from an AddAmendment and doesn't really count as an
+        /// paragraph or if it is from a move amendment and is the paragraph where the orignal should be moved to.
+        /// </summary>
         public bool IsVirtual { get; set; } = false;
-        public string Text { get; set; } = "";
+
+        private string _text;
+        public string Text
+        {
+            get => this._text;
+            set
+            {
+                if (_text != value) return;
+                var oldText = this._text;
+                this._text = value;
+                this.TextChanged?.Invoke(this, value, oldText);
+            }
+        }
         public bool Visible { get; set; } = true;
         public bool Corrected { get; set; }//ADDED ~Felix
 
