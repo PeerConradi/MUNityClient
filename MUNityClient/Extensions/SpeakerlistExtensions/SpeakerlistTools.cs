4ef9ca1327331198d109cc297044f7334e49a4fb
Aeuke Peer Conradi <peerconradi@gmail.com>
2020-12-13 00:23:01 +0100

    First Speakerlist implementation

---
+++ MUNityClient/Extensions/SpeakerlistExtensions/SpeakerlistTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MUNityClient.Models.ListOsSpeakers;
+
+namespace MUNityClient.Extensions.SpeakerlistExtensions
+{
+    public static class SpeakerlistTools
+    {
+        public static void AddSpeaker(this ListOfSpeakers listOfSpeakers, Speaker speaker)
+        {
+            listOfSpeakers.Speakers.Add(speaker);
+        }
+    }
+}
