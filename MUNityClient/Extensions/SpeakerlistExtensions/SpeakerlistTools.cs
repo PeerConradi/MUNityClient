d86760167866a588715ada22702e5c84f3323b75
Aeuke Peer Conradi <peerconradi@gmail.com>
2020-12-14 21:35:17 +0100

    Typo fixed

--- MUNityClient/Extensions/SpeakerlistExtensions/SpeakerlistTools.cs
+++ MUNityClient/Extensions/SpeakerlistExtensions/SpeakerlistTools.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MUNityClient.Models.ListOsSpeakers;
+using MUNityClient.Models.ListOfSpeakers;
 
 namespace MUNityClient.Extensions.SpeakerlistExtensions
 {
