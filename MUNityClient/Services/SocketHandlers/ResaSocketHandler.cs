1ef741189738a09c2bb08c3232c451da5c8f9f26
Aeuke Peer Conradi <peerconradi@gmail.com>
2020-12-30 19:54:24 +0100

    Implemented TypedSocket in SockenHandler

--- MUNityClient/Services/SocketHandlers/ResaSocketHandler.cs
+++ MUNityClient/Services/SocketHandlers/ResaSocketHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using MUNity.Hubs;
 using MUNity.Models.Resolution;
 using System;
 using System.Collections.Generic;
@@ -7,11 +8,14 @@ using System.Threading.Tasks;
 
 namespace MUNityClient.Services.SocketHandlers
 {
-    public class ResaSocketHandler
+    /// <summary>
+    /// The ResaSocketHandler is connecting with the SignalR Socket of the MUNity API.
+    /// </summary>
+    public class ResaSocketHandler : MUNity.Hubs.ITypedResolutionHub
     {
         public delegate void OnResolutionChanged(Resolution resolution);
 
-        public event OnResolutionChanged ResolutionChanged;
+        public event OnResolutionChanged SocketResolutionChanged;
 
         public HubConnection HubConnection { get; set; }
 
@@ -25,10 +29,9 @@ namespace MUNityClient.Services.SocketHandlers
             IgnoreTransactions = new List<string>();
 
             HubConnection = new HubConnectionBuilder().WithUrl($"{Program.API_URL}/resasocket").Build();
-
-            HubConnection.On<Resolution>("ResolutionChanged", (newResolution) => SocketResolutionChanged(newResolution));
-            HubConnection.On<string, PreambleParagraph, string>("PreambleParagraphChanged", (resolutionId, paragraph, tan) => SocketPreambleParagraphChanged(resolutionId, paragraph, tan));
-            HubConnection.On<string, OperativeParagraph, string>("OperativeParagraphChanged", (resolutionId, paragraph, tan) => SocketOperativeParagraphChanged(resolutionId, paragraph, tan));
+            HubConnection.On<Resolution>(nameof(ResolutionChanged), (newResolution) => ResolutionChanged(newResolution));
+            HubConnection.On<string, PreambleParagraph, string>(nameof(PreambleParagraphChanged), (resolutionId, paragraph, tan) => PreambleParagraphChanged(resolutionId, paragraph, tan));
+            HubConnection.On<string, OperativeParagraph, string>(nameof(OperativeParagraphChanged), (resolutionId, paragraph, tan) => OperativeParagraphChanged(resolutionId, paragraph, tan));
         }
 
         public static async Task<ResaSocketHandler> CreateHandler(Resolution resolution)
@@ -38,49 +41,52 @@ namespace MUNityClient.Services.SocketHandlers
             return instance;
         }
 
-        private void SocketResolutionChanged(Resolution newResolution)
+        public Task ResolutionChanged(Resolution newResolution)
         {
-            if (newResolution.ResolutionId != _resolution.ResolutionId) return;
+            if (newResolution.ResolutionId != _resolution.ResolutionId) return null;
+            // TODO: Tan check
 
             _resolution.Header = newResolution.Header ?? _resolution.Header;
             _resolution.Preamble = newResolution.Preamble ?? _resolution.Preamble;
             _resolution.OperativeSection = newResolution.OperativeSection ?? _resolution.OperativeSection;
-            
-            ResolutionChanged?.Invoke(this._resolution);
+            return null;
         }
 
-        private void SocketPreambleParagraphChanged(string resolutionId, PreambleParagraph newParagraph, string tan)
+        public Task PreambleParagraphChanged(string resolutionId, PreambleParagraph para, string tan)
         {
-            if (IgnoreTransactions.Any(n => n == tan))
-            {
-                IgnoreTransactions.Remove(tan);
-                return;
-            }
+            if (CanIgnore(tan)) return null;
 
-            if (resolutionId != _resolution.ResolutionId) return;
-            var targetParagraph = _resolution.Preamble.Paragraphs.FirstOrDefault(n => n.PreambleParagraphId == newParagraph.PreambleParagraphId);
+            if (resolutionId != _resolution.ResolutionId) return null;
+            var targetParagraph = _resolution.Preamble.Paragraphs.FirstOrDefault(n => n.PreambleParagraphId == para.PreambleParagraphId);
             if (targetParagraph != null)
             {
-                targetParagraph.Text = newParagraph.Text;
-                targetParagraph.Comments = newParagraph.Comments;
+                targetParagraph.Text = para.Text;
+                targetParagraph.Comments = para.Comments;
             }
-            ResolutionChanged?.Invoke(this._resolution);
+            return null;
         }
 
-        private void SocketOperativeParagraphChanged(string resolutionId, OperativeParagraph changedParagraph, string tan)
+        public Task OperativeParagraphChanged(string resolutionId, OperativeParagraph para, string tan)
+        {
+            if (CanIgnore(tan)) return null;
+
+            if (resolutionId != _resolution.ResolutionId) return null;
+            var paragraph = _resolution.OperativeSection.Paragraphs.FirstOrDefault(n => n.OperativeParagraphId == para.OperativeParagraphId);
+            if (paragraph == null) return null;
+            paragraph.Text = para.Text;
+            paragraph.Comments = para.Comments;
+            //ResolutionChanged?.Invoke(this._resolution);
+            return null;
+        }
+
+        private bool CanIgnore(string tan)
         {
             if (IgnoreTransactions.Any(n => n == tan))
             {
                 IgnoreTransactions.Remove(tan);
-                return;
+                return true;
             }
-
-            if (resolutionId != _resolution.ResolutionId) return;
-            var paragraph = _resolution.OperativeSection.Paragraphs.FirstOrDefault(n => n.OperativeParagraphId == changedParagraph.OperativeParagraphId);
-            if (paragraph == null) return;
-            paragraph.Text = changedParagraph.Text;
-            paragraph.Comments = changedParagraph.Comments;
-            ResolutionChanged?.Invoke(this._resolution);
+            return false;
         }
     }
 }
