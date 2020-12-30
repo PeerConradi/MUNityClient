454a4c1d2a39433acd102aa7e2ceb8412e7597d6
Felix <>
2020-12-30 16:25:59 +0100

    Revert "merge ui rework into master"
    This reverts commit 55e5c2178fd50b00faab69d03e0b46fc40fbd629, reversing
    changes made to 56d3f33c79931e3e9df1b42c6ff0641173040626.

--- MUNityClient/Models/Simulation/SimulationUser.cs
+++ MUNityClient/Models/Simulation/SimulationUser.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 namespace MUNityClient.Models.Simulation
 {
+
+    [Obsolete("Use the MUNityBase /MUNitySchema Package!")]
     public class SimulationUser
     {
         public int SimulationUserId { get; set; }
